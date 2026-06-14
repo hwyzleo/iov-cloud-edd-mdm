dfd6e74947e1e2a1c133a3e9cd29709f7db0df77
叶荣杰 <rongjie.ye@roxmotor.com>
2026-06-14 18:41:00 +0800

    refactor: remove SOFTWARE and ASSEMBLY from PartType enum (MDM-DSN-CR-024)

--- edd-mdm-service/src/main/java/net/hwyz/iov/cloud/edd/mdm/service/domain/model/valueobject/PartType.java
+++ edd-mdm-service/src/main/java/net/hwyz/iov/cloud/edd/mdm/service/domain/model/valueobject/PartType.java
@@ -2,13 +2,14 @@ package net.hwyz.iov.cloud.edd.mdm.service.domain.model.valueobject;
 
 /**
  * 零件类型枚举（Material 子域）
+ * 物料性质/类型维度：RAW_MATERIAL / STANDARD_PART / CUSTOM_PART
+ * 软硬件判定：由 is_software 布尔字段承载
+ * 总成判定：由 is_assembly 布尔字段承载
  *
  * @author hwyz_leo
  */
 public enum PartType {
     RAW_MATERIAL,
     STANDARD_PART,
-    CUSTOM_PART,
-    SOFTWARE,
-    ASSEMBLY
+    CUSTOM_PART
 }
